c523f0a75f6abe78333c4b5a9fac1afb1f9bf25e
Dex <dejanloncar907@gmail.com>
2017-07-01 18:47:36 +0200

    Environment created

---
+++ app.js
@@ -0,0 +1,56 @@
+var express       = require('express');
+var path          = require('path');
+var favicon       = require('serve-favicon');
+var logger        = require('morgan');
+var cookieParser  = require('cookie-parser');
+var bodyParser    = require('body-parser');
+var mongoose      = require('mongoose'); // Working with MongoDB
+
+var appRoutes = require('./routes/app');
+var app       = express();
+mongoose.connect('localhost/node-angular'); 
+
+
+/*=================================
+  View engine setup
+===================================*/
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/*==================================
+    Access from different server
+====================================*/
+// If Angular is run on different server than backend
+// It's not allowed by default - Ako hocemo da pristupimo, moramo uneti kod ispod (dozvoljavamo putem middleware-a pristup sa bilo koje lokacije)
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Dozvoljavamo pristup na ovaj server bilo kom drugom domenu
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With', 'Accept'); // Heder koji je dozvoljen (može i bez ovog)
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS'); // Dozvoljene metode
+    next(); // Nastavi sa izvršavanjem kako bi request dosao do rute
+});
+
+// Target Routes
+app.use('/', appRoutes);
+
+
+/*=================================
+    Catch 404 and forward to 
+    error handler
+===================================*/
+app.use(function(req, res, next) {
+  res.render('index');
+});
+
+
+
+
+
+module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,38 @@
+var mongoose          = require('mongoose');
+var Schema            = mongoose.Schema;
+var mUniqueValidator  = require('mongoose-unique-validator');
+
+
+// Blueprint for Message Table
+var schema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    messages: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Message'
+    }]
+});
+
+
+// Use plugin to make unique validation (installed before)
+schema.plugin(mUniqueValidator);
+
+
+module.exports = mongoose.model(
+    'User',
+    schema
+);
