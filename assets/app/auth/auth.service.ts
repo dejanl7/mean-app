b96a217ebdcfc192019fc69b597b03fe0db6c0b0
Dex <dejanloncar907@gmail.com>
2017-07-06 12:03:36 +0200

    original deployment

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -19,7 +19,7 @@ export class AuthService {
     signup(user: User) {
         const body = JSON.stringify(user);
         const headers = new Headers({'Content-Type': 'application/json'});
-        return this.http.post(' http://simple.sdl-profile.net/user', body, {headers: headers})
+        return this.http.post(' https://angular4-deployments.herokuapp.com/user', body, {headers: headers})
             .map((response: Response) => response.json())
             .catch((error: Response) => {
                 this.errorService.handleError(error.json());
@@ -30,7 +30,7 @@ export class AuthService {
     signin(user: User) {
         const body = JSON.stringify(user);
         const headers = new Headers({'Content-Type': 'application/json'});
-        return this.http.post(' http://simple.sdl-profile.net/user/signin', body, {headers: headers})
+        return this.http.post(' https://angular4-deployments.herokuapp.com/user/signin', body, {headers: headers})
             .map((response: Response) => response.json())
             .catch((error: Response) => {
                     this.errorService.handleError(error.json());
